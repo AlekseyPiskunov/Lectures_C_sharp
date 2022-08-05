76ca4115c52a9eb532f27c742c2041fc7f02e9e6
Alexey <alexey_beerman@mail.ru>
2022-08-05 19:28:20 +0300

    add Exemple Friends And Do

---
+++ Exemple_FriendsAndDog/Program.cs
@@ -0,0 +1,25 @@
+﻿int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+double distance = 10000;
+double time = 0;
+int count = 0;
+int friend = 2;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    if (friend == 2)
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count = count + 1;
+}
+Console.Write("Собака пробежит раз = ");
+Console.WriteLine(count);
