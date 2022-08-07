cfb8ce4db20700677d2f7e47da1b16b1e2c17b02
Alexey <alexey_beerman@mail.ru>
2022-08-07 18:54:13 +0300

    add Exemple Method Array

---
+++ Exemple_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 };
+
+int n = array.Length; // n - длина массива
+int find = 18; // будем искать элемент 4
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine($"Индекс - {index}");
+        break;
+    }
+    index++;
+}
