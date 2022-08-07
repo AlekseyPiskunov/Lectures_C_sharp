3a7922efd569e92ee67d96b8972b78646869fbc8
Alexey <alexey_beerman@mail.ru>
2022-08-07 20:00:59 +0300

    add Exemple Array Library

---
+++ Exemple_ArrayLibrary/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.Clear();
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+// Рандомно заполняет массив
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+//печатает каждый элемент
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+// Будет искать индекс 
+int[] array = new int[10]; // Создал массив с 10-ю элементами.
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4); // показывает индекс указанного элемента(4)
+Console.WriteLine(pos);
